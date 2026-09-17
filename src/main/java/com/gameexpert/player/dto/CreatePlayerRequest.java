a27e406520c00003ab93c16b07898495690b9d44
dingokim2 <alvin0406@naver.com>
2026-09-17 10:47:54 +0900

    Lv 3. 요청 검증과 DTO: 플레이어 등록

--- src/main/java/com/gameexpert/player/dto/CreatePlayerRequest.java
+++ src/main/java/com/gameexpert/player/dto/CreatePlayerRequest.java
@@ -1,11 +1,15 @@
 package com.gameexpert.player.dto;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class CreatePlayerRequest {
 
     // TODO Lv 3: 2~12글자의 영문 대소문자, 숫자와 밑줄을 허용하는 검증을 적용합니다.
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$")
+    @Size(min=2, max=12)
     private final String nickname;
 
     public CreatePlayerRequest(String nickname) {
